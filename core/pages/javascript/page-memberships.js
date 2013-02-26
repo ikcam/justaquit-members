d6f1da39e682b0333201a2bda57881d01209299b
Irving Kcam <xploit29@gmail.com>
2013-02-26 08:25:01 -0500

    Manage memberships JS

--- core/pages/javascript/page-memberships.js
+++ core/pages/javascript/page-memberships.js
@@ -6,30 +6,43 @@ jQuery(document).ready(function($){
 	});
 
 	$( '#membership-add' ).submit(function(){
+		var parent = $(this);
+
+		$(this).find('.waiting').show();
+		$(this).find('#name').prop('disabled',true);
+		$(this).find('#submit').prop('disabled',true);
+
 		var data = {
 			action: 'jmembers_membership_add',
-			name: $(this).find('#name').attr('value'),
-			nonce: $(this).find('#jmember_nonce').attr('value')
+			name: $(this).find('#name').val(),
+			nonce: $(this).find('#jmember_nonce').val()
 		}
 
 		$.post( ajaxurl, data, function(response){
 			if( response == 1 ){
-				$(this).find('#name').attr('value','');
 				location.reload();
 			} else {
 				alert( response );
+				$(parent).find('#name').prop('disabled',false);
+				$(parent).find('#name').focus();
+				$(parent).find('#submit').prop('disabled',false);
+				$(parent).find('.waiting').hide();
 			}
 		});
 
 		return false;
 	});
 
-	$( '#membership-delete' ).click(function(){
-		var membership_id = $(this).attr('rel');
+	$( 'form[id^="membership-delete-"]' ).submit(function(){
+		var parent = $(this);
+
+		$(this).find('#submit').prop('disabled',true);
+		$(this).find('.waiting').show();
 
 		var data = {
 			action: 'jmembers_membership_delete',
-			membership_id: membership_id
+			nonce: $(this).find('#jmember_nonce').val(),
+			membership_id: $(this).find('#membership_id').val()
 		}
 
 		$.post( ajaxurl, data, function(response){
@@ -37,7 +50,27 @@ jQuery(document).ready(function($){
 				location.reload();
 			} else {
 				alert(response);
+				$(parent).find('#submit').prop('disabled',false);
+				$(this).find('.waiting').hide();
 			}
+		});
+
+		return false;
+	});
+
+	$( 'form[id^="package-add-"]' ).submit(function(){
+		var parent = $(this);
+
+		$(this).find('#submit').prop('disabled',true);
+		$(this).find('.waiting').show();
+
+		var data = {
+			action: 'jmembers_package_add',
+			nonce: $(this).find('#jmember_nonce').val()
+		}
+
+		$.post(ajaxurl, data, function(response){
+			alert(response);
 		});
 	});
 });
