153116bb5dd7a9f397eac1584ef7c6b809fba90e
Irving Kcam <xploit29@gmail.com>
2013-04-09 15:56:43 -0500

    Add required class

--- justaquit-members.php
+++ justaquit-members.php
@@ -11,6 +11,7 @@ License: GPL2
 
 define( 'JMEMBERS_PATH' , plugin_dir_path( __FILE__ ));
 define( 'JMEMBERS_URL', plugin_dir_url( __FILE__ ) );
+require( JMEMBERS_PATH . 'core/classes/Member.php' );
 require( JMEMBERS_PATH . 'core/classes/Membership.php' );
 require( JMEMBERS_PATH . 'core/classes/Package.php' );
 require( JMEMBERS_PATH . 'core/classes/Transaction.php' );
